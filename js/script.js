39605e49a5cde3288ef93b8f91f6824bdd695c87
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-01 14:58:08 +0200

    add  script + css after simbol

--- js/script.js
+++ js/script.js
@@ -11,5 +11,33 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 L'output del prezzo finale va stampato in forma umana (con massimo due decimali,
      per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
  */
+const unity= 0.21;
+var distance=parseInt(prompt("Quanti chilometri devi percorrere?: "));
+var age=parseInt(prompt(" Inserire l'età: " ));
+var price;
 
-console.log("here");
+// write on html
+var pDistance= document.getElementById('distance').innerText=distance;
+var pAge= document.getElementById('age').innerText=age;
+var total= document.getElementById('total');
+//debug inline
+console.log("distanza: "+distance,typeof distance);
+console.log("età: ",age,typeof distance);
+
+//multiply unity and distance input user;
+price=unity*distance;
+
+//debug inline
+console.log("imponibile: ",price,typeof price);
+
+//discount under 18
+if (age<18){
+    price+=((price*20)/100);
+}else if (age>= 65){        //discount over 65
+    price+=((price*40)/100);
+}
+
+//debug inline
+console.log("il prezzo totale è :", price, typeof price);
+
+total.innerText=parseFloat(price).toFixed(2);
